1b0f57259234db566d67fc5d6ff9f5e3279b9aa8
FernandoVSN <91626674+FernandoVSN@users.noreply.github.com>
2022-01-31 16:13:15 -0300

    Add files via upload

---
+++ DIO_Projeto_Series/Enum/Genero.cs
@@ -0,0 +1,16 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Documentario = 4,
+		Docuficcao = 5,
+		Drama = 6,
+		Fantasia = 7,
+		Musical = 8,
+		Romance = 9,
+		Terror = 10
+	}
+}
